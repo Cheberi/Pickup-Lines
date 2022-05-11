8d082280469a59b55a5ef4023a19e7f9c865665b
Cheberi <meshackkemboi011@gmail.com>
2022-05-11 16:07:35 +0300

    updated config file

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
